45153b672b520463002e62a558c104da07b623ab
riyasyjan <syjanriya31@gmail.com>
2026-06-20 12:15:01 +0530

    Program 10

---
+++ IsPrime_Prog10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class IsPrime_Prog10{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+        System.out.println(n + " is a Prime number");
+        }
+        else{
+            System.out.println(n + " is not a Prime number");
+        }
+    }
+}
