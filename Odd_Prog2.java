ac5863b7f67be73664d7d167032f7f9b7f1b0982
riyasyjan <syjanriya31@gmail.com>
2026-06-20 11:18:00 +0530

    Program 1

---
+++ Odd_Prog2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Odd_Prog2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2 != 0){
+            System.out.println("The number is Odd");
+        }
+    }
+}
